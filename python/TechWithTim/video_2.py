dd131fb8ea42fa07f326f1019a25fee105f80dc7
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-03-25 19:27:07 -0500

    NBA player Salaries web scrapping added

--- python/TechWithTim/video_2.py
+++ python/TechWithTim/video_2.py
@@ -11,4 +11,4 @@ search = driver.find_element_by_name('q')
 search.send_keys('Youngermaster')
 search.send_keys(Keys.ENTER)
 
-print(driver.page_source)
+# print(driver.page_source) # Prints all the page source code.
