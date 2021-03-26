952c7479c87d22f9eba405c326be4899e57de5cf
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-03-26 12:01:49 -0500

    GetAClub scripts added

---
+++ GetAClub_platform_button_clicker.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+PATH = 'C:\Program Files (x86)\chromedriver.exe'
+driver = webdriver.Chrome(PATH)
+
+driver.get('https://geta-client-webapp-dev.azurewebsites.net/#/landing')
+
+button = driver.find_element_by_xpath('/html/body/genesys-root/genesys-landing-page/section/div/div/div/swiper/div/div[1]/div[2]/div/div[3]/button[1]')
+button.click()
+submit = driver.find_element_by_xpath('/html/body/genesys-root/genesys-signup-page/section/div/div[2]/div/form/div/div[2]/button')
+submit.click()
