8bcb88d4edcb28cdaa41122d79238cb8dccb6f2c
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-03-27 13:00:49 -0500

    requirements.txt added

--- GetAClub/Tests/title_checker.py
+++ GetAClub/Tests/title_checker.py
@@ -4,8 +4,6 @@ PATH = 'C:\Program Files (x86)\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 
 driver.get('https://geta-client-webapp-dev.azurewebsites.net/#/landing')
-#print(driver.title)
-if driver.title == 'GETA Club Play - Genesys':
-    print('[Success]')
-else:
-    print('[Error]')
+
+def test_answer():
+    assert driver.title == 'GETA Club Play - Genesys'
