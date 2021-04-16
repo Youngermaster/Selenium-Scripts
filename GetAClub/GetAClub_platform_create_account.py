e3b5014c187c1525a34af960d13b56054fa43ef5
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-04-15 20:53:02 -0500

    Create account refactored

--- GetAClub/GetAClub_platform_create_account.py
+++ GetAClub/GetAClub_platform_create_account.py
@@ -12,10 +12,23 @@ import names
 def click_element_by_xpath(xpath):
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, xpath))).click()
 
-if __name__ == "__main__":
-    PATH = 'C:\Program Files (x86)\chromedriver.exe'
-    driver = webdriver.Chrome(PATH)
+def generate_user():
+    name = names.get_last_name()
+    email = name + '@gmail.com'
+    password = get_simple_password(10)
 
+    file = open("emailsAndPasswords.txt", "a")
+    file.write("________________\n")
+    file.write(f"\nName: {name}")
+    file.write(f"\nEmail: {email}")
+    file.write(f"\nPassword: {password}\n")
+    file.write("________________\n")
+    file.close()
+
+    return name, email, password
+
+
+def create_account(driver):
     driver.get('https://geta-client-webapp-dev.azurewebsites.net/#/landing')
 
     # SignUp button
@@ -36,21 +49,8 @@ if __name__ == "__main__":
     click_element_by_xpath('/html/body/genesys-root/genesys-signup-page/section/div/div[2]/div/form/div/div[2]/button')
     # Click Avatar
     click_element_by_xpath('/html/body/genesys-root/genesys-signup-page/section/div/div[2]/div/genesys-gallery-form/section/div/div[3]/div[2]/div')
-
-    name = names.get_last_name()
-    email = name + '@gmail.com'
-    password = get_simple_password(10)
-
-    print(f'Password -> {password}')
-
-    file = open("emailsAndPasswords.txt", "a")
-    file.write("________________\n")
-    file.write(f"\nName: {name}")
-    file.write(f"\nEmail: {email}")
-    file.write(f"\nPassword: {password}\n")
-    file.write("________________\n")
-    file.close()
-
+    
+    name, email, password = generate_user()
 
     full_name_input = driver.find_element_by_xpath('/html/body/genesys-root/genesys-signup-page/section/div/div[2]/div/div/form/input[1]')
     full_name_input.clear()
@@ -63,3 +63,10 @@ if __name__ == "__main__":
     password_input.send_keys(password)
     password_input.send_keys(Keys.RETURN)
 
+
+if __name__ == "__main__":
+    PATH = 'C:\Program Files (x86)\chromedriver.exe'
+    driver = webdriver.Chrome(PATH)
+    create_account(driver)
+    
+    
