5d57ab27d0c51d97f6f91dbced741cf3c8723744
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-04-16 08:55:49 -0500

    💚Skip tutorial added

--- GetAClub/Genesys/create_account.py
+++ GetAClub/Genesys/create_account.py
@@ -9,15 +9,19 @@ from generate_password import get_simple_password
 # Use pip install names
 import names
 
+
 def click_element_by_xpath(xpath):
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, xpath))).click()
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.XPATH, xpath))).click()
+
 
 def generate_user():
-    name = names.get_last_name()
+    name = f'{names.get_last_name()}{names.get_first_name()}'
     email = name + '@gmail.com'
     password = get_simple_password(10)
 
     return name, email, password
+
 
 def store_user(name, email, password):
     file = open("emailsAndPasswords.txt", "a")
@@ -29,48 +33,63 @@ def store_user(name, email, password):
     file.close()
 
 
-
 def create_account(driver):
     driver.get('https://geta-client-webapp-dev.azurewebsites.net/#/landing')
 
     # SignUp button
-    click_element_by_xpath('/html/body/genesys-root/genesys-landing-page/section/div/div/div/swiper/div/div[1]/div[2]/div/div[3]/button[1]')
+    click_element_by_xpath(
+        '/html/body/genesys-root/genesys-landing-page/section/div/div/div/swiper/div/div[1]/div[2]/div/div[3]/button[1]')
     # Date selector
-    click_element_by_xpath('/html/body/genesys-root/genesys-signup-page/section/div/div[2]/div/form/div/div[1]/input')
+    click_element_by_xpath(
+        '/html/body/genesys-root/genesys-signup-page/section/div/div[2]/div/form/div/div[1]/input')
     # Year selection
-    click_element_by_xpath('/html/body/div[3]/div[2]/div/mat-dialog-container/mat-datepicker-content/mat-calendar/mat-calendar-header/div/div/button[1]')
+    click_element_by_xpath(
+        '/html/body/div[3]/div[2]/div/mat-dialog-container/mat-datepicker-content/mat-calendar/mat-calendar-header/div/div/button[1]')
     # Backwards arrow button
-    click_element_by_xpath('/html/body/div[3]/div[2]/div/mat-dialog-container/mat-datepicker-content/mat-calendar/mat-calendar-header/div/div/button[2]')
+    click_element_by_xpath(
+        '/html/body/div[3]/div[2]/div/mat-dialog-container/mat-datepicker-content/mat-calendar/mat-calendar-header/div/div/button[2]')
     # Selects the year, in this case 2001
-    click_element_by_xpath('/html/body/div[3]/div[2]/div/mat-dialog-container/mat-datepicker-content/mat-calendar/div/mat-multi-year-view/table/tbody/tr[3]/td[2]/div[1]')
+    click_element_by_xpath(
+        '/html/body/div[3]/div[2]/div/mat-dialog-container/mat-datepicker-content/mat-calendar/div/mat-multi-year-view/table/tbody/tr[3]/td[2]/div[1]')
     # Selects the month, in this case march
-    click_element_by_xpath('/html/body/div[3]/div[2]/div/mat-dialog-container/mat-datepicker-content/mat-calendar/div/mat-year-view/table/tbody/tr[2]/td[3]/div[1]')
+    click_element_by_xpath(
+        '/html/body/div[3]/div[2]/div/mat-dialog-container/mat-datepicker-content/mat-calendar/div/mat-year-view/table/tbody/tr[2]/td[3]/div[1]')
     # Selects the day, in this case 19
-    click_element_by_xpath('/html/body/div[3]/div[2]/div/mat-dialog-container/mat-datepicker-content/mat-calendar/div/mat-month-view/table/tbody/tr[4]/td[2]/div[1]')
+    click_element_by_xpath(
+        '/html/body/div[3]/div[2]/div/mat-dialog-container/mat-datepicker-content/mat-calendar/div/mat-month-view/table/tbody/tr[4]/td[2]/div[1]')
     # Clicks submit
-    click_element_by_xpath('/html/body/genesys-root/genesys-signup-page/section/div/div[2]/div/form/div/div[2]/button')
+    click_element_by_xpath(
+        '/html/body/genesys-root/genesys-signup-page/section/div/div[2]/div/form/div/div[2]/button')
     # Click Avatar
-    click_element_by_xpath('/html/body/genesys-root/genesys-signup-page/section/div/div[2]/div/genesys-gallery-form/section/div/div[3]/div[2]/div')
-    
+    click_element_by_xpath(
+        '/html/body/genesys-root/genesys-signup-page/section/div/div[2]/div/genesys-gallery-form/section/div/div[3]/div[2]/div')
+
     name, email, password = generate_user()
 
     store_user(name, email, password)
 
-    full_name_input = driver.find_element_by_xpath('/html/body/genesys-root/genesys-signup-page/section/div/div[2]/div/div/form/input[1]')
+    full_name_input = driver.find_element_by_xpath(
+        '/html/body/genesys-root/genesys-signup-page/section/div/div[2]/div/div/form/input[1]')
     full_name_input.clear()
     full_name_input.send_keys(name)
-    email_input =  driver.find_element_by_xpath('/html/body/genesys-root/genesys-signup-page/section/div/div[2]/div/div/form/input[2]')
+    email_input = driver.find_element_by_xpath(
+        '/html/body/genesys-root/genesys-signup-page/section/div/div[2]/div/div/form/input[2]')
     email_input.clear()
     email_input.send_keys(email)
-    password_input = driver.find_element_by_xpath('/html/body/genesys-root/genesys-signup-page/section/div/div[2]/div/div/form/input[3]')
+    password_input = driver.find_element_by_xpath(
+        '/html/body/genesys-root/genesys-signup-page/section/div/div[2]/div/div/form/input[3]')
     password_input.clear()
     password_input.send_keys(password)
     password_input.send_keys(Keys.RETURN)
+
+
+def skip_tutorial():
+    click_element_by_xpath(
+        '/html/body/genesys-root/genesys-master-page/genesys-ctrl-tour/div/a')
 
 
 if __name__ == "__main__":
     PATH = 'C:\Program Files (x86)\chromedriver.exe'
     driver = webdriver.Chrome(PATH)
     create_account(driver)
-    
-    
+    skip_tutorial()
