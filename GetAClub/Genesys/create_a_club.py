39ca299b1f95b72bb0b980506e8fcbbe1afc329a
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-04-19 16:58:20 -0500

    Create a Club added

--- GetAClub/Genesys/create_a_club.py
+++ GetAClub/Genesys/create_a_club.py
@@ -24,7 +24,31 @@ def create_a_club():
         '/html/body/genesys-root/genesys-game-clubs-page/section/div[2]/div/div/div[2]/genesys-clubs-create-club-page/section/genesys-gallery-form/section/div/div[3]/div[1]/div/img')
 
     driver.find_element_by_xpath(
-        '/html/body/genesys-root/genesys-game-clubs-page/section/div[2]/div/div/div[2]/genesys-clubs-create-club-page/section/form/div/div[2]/input').send_keys()
+        '/html/body/genesys-root/genesys-game-clubs-page/section/div[2]/div/div/div[2]/genesys-clubs-create-club-page/section/form/div/div[2]/input'
+    ).send_keys('TheSeleniumClub')
+
+    driver.find_element_by_xpath(
+        '/html/body/genesys-root/genesys-game-clubs-page/section/div[2]/div/div/div[2]/genesys-clubs-create-club-page/section/form/div/div[3]/input'
+    ).send_keys("hisoka")
+
+    driver.find_element_by_xpath(
+        '/html/body/genesys-root/genesys-game-clubs-page/section/div[2]/div/div/div[2]/genesys-clubs-create-club-page/section/form/div/div[4]/input'
+    ).send_keys('The amazing Selenium')
+
+    # * Creates the club
+    click_element_by_xpath(
+        '/html/body/genesys-root/genesys-game-clubs-page/section/div[2]/div/div/div[2]/genesys-clubs-create-club-page/section/form/div/div[5]/button'
+    )
+
+    # * Clicks success message
+    click_element_by_xpath(
+        '/html/body/div[2]/div[4]/div/mat-dialog-container/genesys-ctrl-notification/div/button'
+    )
+
+    # * Clicks error message
+    click_element_by_xpath(
+        '/html/body/div[2]/div[2]/div/mat-dialog-container/genesys-ctrl-notification/div/button'
+    )
 
 
 if __name__ == "__main__":
