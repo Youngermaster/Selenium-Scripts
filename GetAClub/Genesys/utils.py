f8f8ec6bb4cc8500052c285d3170248c8bc7c54e
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-04-23 15:36:28 -0500

    GetA club codes deleted

--- GetAClub/Genesys/utils.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-import names
-
-PATH = 'C:\Program Files (x86)\chromedriver.exe'
-DRIVER = webdriver.Chrome(PATH)
-
-def create_club_name():
-    return names.get_full_name()
