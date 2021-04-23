1611bf6e7cc82b0f59f30d2ff76abcd9a07a0ed3
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-04-23 13:10:40 -0500

    Virtual display added

--- Tests/title_checker.py
+++ Tests/title_checker.py
@@ -1,9 +1,12 @@
+from pyvirtualdisplay import Display
 import pytest
 from selenium import webdriver
 driver = webdriver.Chrome()
 
 
 def test_answer():
+    display = Display(visible=0, size=(800, 800))  
+    display.start()
     driver.get('https://www.google.com')
     print(driver.title)
     assert driver.title == 'Google'
