ff83463765acacc14165db45b817c1b381839047
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-04-26 23:31:33 -0500

    Another Absolute PATHs deleted

--- python/TechWithTim/video_2.py
+++ python/TechWithTim/video_2.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-PATH = 'C:\Program Files (x86)\chromedriver.exe'
-driver = webdriver.Chrome(PATH)
+driver = webdriver.Chrome()
 
 driver.get('https://www.google.com')
 print(driver.title)
